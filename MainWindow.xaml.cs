7494c58a0c75b188f5226e801e0d2f75432e1b3b
PhilippWuelfing1 <philippwuelfing@web.de>
2026-05-31 18:01:52 +0200

    Snake Koordinaten

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,4 +1,7 @@
-﻿using System.Text;
+﻿using System.ComponentModel;
+using System.Diagnostics;
+using System.Globalization;
+using System.Text;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
@@ -12,86 +15,177 @@ using System.Windows.Threading;
 
 namespace SnakeAsync
 {
-    public partial class MainWindow : Window
+    public partial class MainWindow : Window, INotifyPropertyChanged
     {
         private CancellationTokenSource _cts = new CancellationTokenSource();
-        Polyline _polyline = new Polyline();
+        //Polyline _polyline = new Polyline();
+        Rectangle _food = new Rectangle();
+        private string? _snakeHead_x;
+        private string? _snakeHead_y;
 
         public MainWindow()
         {
             InitializeComponent();
-            //canv.Focus();                        
-            BuildLine();
+            this.DataContext = this;
+            var head = snake.Points.Last(); 
+            SnakeHead_x = head.X.ToString(); 
+            SnakeHead_y = head.Y.ToString();
+            //BuildSnake();
+            //spawnFood();
+            canv.Focus();
         }
 
-        private void BuildLine()
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+
+
+        public string? SnakeHead_x
+        {
+            get
+            {
+                return snake == null ? "0" : snake.Points.LastOrDefault().X.ToString(); 
+            }
+            set
+            {
+                _snakeHead_x = snake.Points.LastOrDefault().X.ToString(); 
+                OnPropertyChanged(nameof(SnakeHead_x));
+            }
+        }
+
+        public string? SnakeHead_y
+        {
+            get
+            {
+                return snake == null ? "0" : snake.Points.LastOrDefault().Y.ToString();
+            }
+            set
+            {
+                _snakeHead_y = snake.Points.LastOrDefault().Y.ToString();
+                OnPropertyChanged(nameof(SnakeHead_y));
+            }
+        }
+
+        private void BuildSnake()
         {
             // Create the Polyline element
-            _polyline.Stroke = Brushes.Black;
-            _polyline.StrokeThickness = 10;
+            //snake = _polyline;
+            //_polyline.Stroke = Brushes.Black;
+            //_polyline.StrokeThickness = 10;
 
             // Define the coordinates
             PointCollection points = new PointCollection();
             points.Add(new Point(100, 100));
+            points.Add(new Point(100, 110));
+            points.Add(new Point(100, 120));
+            points.Add(new Point(100, 130));
+            points.Add(new Point(100, 140));
+            points.Add(new Point(100, 150));
+            points.Add(new Point(100, 160));
+            points.Add(new Point(100, 170));
+            points.Add(new Point(100, 180));
+            points.Add(new Point(100, 190));
             points.Add(new Point(100, 200));
 
-            _polyline.Points = points;
-            canv.Children.Add(_polyline);
+            //_polyline.Points = points;
+            //canv.Children.Add(_polyline);
         }
+
+        
 
         private async void canv_PreviewKeyDown(object sender, KeyEventArgs e)
         {
             if (e.Key == Key.D)
             {
                 ResetToken();
-                _ = move(new Point(10, 0), canv, _polyline, _cts.Token);
-                //_ = GrowingRightAsync(canv, _polyline, _cts.Token);
+                _ = move(new Point(10, 0), canv, snake, _cts.Token);
                 e.Handled = true;
             }
             else if (e.Key == Key.S)
             {
                 ResetToken();
-                _ = move(new Point(0, 10), canv, _polyline, _cts.Token);
+                _ = move(new Point(0, 10), canv, snake, _cts.Token);
                 e.Handled = true;
             }
 
             else if (e.Key == Key.A)
             {
                 ResetToken();
-                _ = move(new Point(-10, 0), canv, _polyline, _cts.Token);
+                _ = move(new Point(-10, 0), canv, snake, _cts.Token);
                 e.Handled = true;
             }
             else if (e.Key == Key.W)
             {
                 ResetToken();
-                _ = move(new Point(0, -10), canv, _polyline, _cts.Token);
+                _ = move(new Point(0, -10), canv, snake, _cts.Token);
                 e.Handled = true;
             }
         }
 
-        private async Task move(Point direction, Canvas canv, Polyline _polyline, CancellationToken token)
+        private async Task move(Point direction, Canvas canv, Polyline snake, CancellationToken token)
         {
             while (true)
             {
                 token.ThrowIfCancellationRequested();
 
-                bool shouldContinue = false;
+                bool shouldContinueV = false;
+                bool shouldContinueH = false;
 
                 await Dispatcher.InvokeAsync(() =>
                 {
-                    var lastPoint = _polyline.Points.LastOrDefault();
-                    shouldContinue = lastPoint.Y < canv.ActualHeight - 400;
+                    var lastPoint = snake.Points.LastOrDefault();
 
-                    if (shouldContinue)
+                    shouldContinueV = lastPoint.Y < canv.ActualHeight - 100 && lastPoint.Y > 100;
+                    shouldContinueH = lastPoint.X < canv.ActualWidth - 100 && lastPoint.X > 100;
+
+                    if (true)
                     {
-                        _polyline.Points.Add(new Point(lastPoint.X + direction.X, lastPoint.Y + direction.Y));
+                        snake.Points.Add(new Point(lastPoint.X + direction.X, lastPoint.Y + direction.Y));
+                        //Letztes Element entfernen, damit die Länge konstant bleibt
+                        //snake.Points.RemoveAt(0);
+
+                        OnPropertyChanged(nameof(SnakeHead_x));
+                        OnPropertyChanged(nameof(SnakeHead_y));
+
                     }
+                    else
+                    {
+
+                    }
+
+                    if(snake.Points.LastOrDefault().X == Canvas.GetLeft(_food) && snake.Points.LastOrDefault().Y == Canvas.GetTop(_food))
+                    {
+                        // Food eaten
+                        canv.Children.Remove(_food);
+                        spawnFood();
+                    }
+
                 }, System.Windows.Threading.DispatcherPriority.Render);
 
-                if (!shouldContinue) break;
+                //if (!shouldContinue) break;
 
                 await Task.Delay(30, token);
             }
+        }
+
+        private void spawnFood()
+        {
+            _food = new Rectangle();
+            _food.Height = 20;
+            _food.Width = 20;
+            _food.Fill = Brushes.Red;
+            _food.StrokeThickness = 10;
+
+            Random rnd = new Random();
+            int topAndLeft = rnd.Next(1, 1300);  
+
+            Canvas.SetTop(_food, topAndLeft);
+            Canvas.SetLeft(_food, topAndLeft);
+
+            canv.Children.Add(_food);
         }
 
         private void ResetToken()
@@ -100,5 +194,19 @@ namespace SnakeAsync
             _cts.Dispose();
             _cts = new CancellationTokenSource();
         }
+
+        private void Window_StateChanged(object sender, EventArgs e)
+        {
+            if (this.WindowState == WindowState.Maximized)
+            {
+                canv.Focus();
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            canv.Focus();
+        }
     }
+
 }
---
+++ ViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+using System.Xml.Linq;
+
+namespace SnakeAsync
+{
+    public class ViewModel : INotifyPropertyChanged
+    {
+        public ViewModel()
+        {
+
+        }
+
+        private string _name = "John Doe";
+
+        public event PropertyChangedEventHandler? PropertyChanged;
+        protected void OnPropertyChanged(string propertyName)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }
+
+        public string PersonName
+        {
+            get { return _name; }
+            set
+            {
+                _name = value;
+                // Call OnPropertyChanged whenever the property is updated
+                OnPropertyChanged(PersonName);
+            }
+        }
+
+    }
+}
